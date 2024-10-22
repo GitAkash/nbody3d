49b320058d9b112213141151cdf30964d99ae71f
Akash Mondal <akash[at]mondal.zip>
2024-10-22 20:33:48 +0200

    added some functions to Vec3D, rmul, sub, truediv.

--- nbody3d.py
+++ nbody3d.py
@@ -5,6 +5,7 @@ import matplotlib
 
 matplotlib.use('TkAgg')
 
+
 # Setup a Vec3D class.
 class Vec3D:
     def __init__(self, x, y, z):
@@ -18,8 +19,18 @@ class Vec3D:
     def __mul__(self, scalar):
         return Vec3D(self.x * scalar, self.y * scalar, self.z * scalar)
 
+    def __rmul__(self, scalar):
+        return Vec3D(self.x * scalar, self.y * scalar, self.z * scalar)
+
+    def __sub__(self, other):
+        return Vec3D(self.x - other.x, self.y - other.y, self.z - other.z)
+
+    def __truediv__(self, scalar):
+        return Vec3D(self.x / scalar, self.y / scalar, self.z / scalar)
+
     def get_length(self):
         return np.sqrt(self.x ** 2 + self.y ** 2 + self.z ** 2)
+
 
 # Setting up particles.
 class Particle:
@@ -39,9 +50,9 @@ class Particle:
         self.acceleration = Vec3D(0, 0, 0)
         for j in range(len(particles)):
             if self.i != j:
-                dir_vec = self.position - particles[j].position
+                dir_vec = particles[j].position - self.position
                 len_vec = dir_vec.get_length()
-                unit_vec = dir_vec / len_vec
+                unit_vec = dir_vec * 1 / len_vec
                 if len_vec == 0:
                     continue
                 f = (self.G * self.mass * particles[j].mass) / len_vec ** 2
@@ -55,6 +66,7 @@ class Particle:
 # Setting up the simulation
 par = []
 par.append(Particle(Vec3D(0, 0, 0), Vec3D(0.5, 0.5, 0.5), 1))
+par.append(Particle(Vec3D(10, 8, 7), Vec3D(0.5, 0.5, 0.5), 1))
 
 n = len(par)
 dt = 1
@@ -67,9 +79,9 @@ ax = fig.add_subplot(projection="3d")
 positions = np.array([[p.position.x, p.position.y, p.position.z] for p in par])
 scat = ax.scatter(positions[:, 0], positions[:, 1], positions[:, 2])
 
-ax.set_xlim(0, 10)
-ax.set_ylim(0, 10)
-ax.set_zlim(0, 10)
+ax.set_xlim(-100, 100)
+ax.set_ylim(-100, 100)
+ax.set_zlim(-100, 100)
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
 ax.set_zlabel('Z')
@@ -77,6 +89,7 @@ ax.set_zlabel('Z')
 
 def init():
     return scat,
+
 
 def update(frame):
     for i in range(n):
@@ -86,6 +99,7 @@ def update(frame):
     positions = np.array([[p.position.x, p.position.y, p.position.z] for p in par])
     scat._offsets3d = (positions[:, 0], positions[:, 1], positions[:, 2])
     return scat,
+
 
 ani = FuncAnimation(fig, update, init_func=init, frames=100, interval=50, blit=False)
 
