e775fcc6322d5c38e077e1a42b2e181cc029f634
Akash Mondal <akash[at]mondal.zip>
2024-10-22 20:22:08 +0200

    Added some functions to vec3d, because i got errors.
    Added frontend so i can see my beautiful particle!

--- nbody3d.py
+++ nbody3d.py
@@ -1,7 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+import matplotlib
 
+matplotlib.use('TkAgg')
 
 # Setup a Vec3D class.
 class Vec3D:
@@ -9,6 +11,12 @@ class Vec3D:
         self.x = x
         self.y = y
         self.z = z
+
+    def __add__(self, other):
+        return Vec3D(self.x + other.x, self.y + other.y, self.z + other.z)
+
+    def __mul__(self, scalar):
+        return Vec3D(self.x * scalar, self.y * scalar, self.z * scalar)
 
     def get_length(self):
         return np.sqrt(self.x ** 2 + self.y ** 2 + self.z ** 2)
@@ -48,6 +56,37 @@ class Particle:
 par = []
 par.append(Particle(Vec3D(0, 0, 0), Vec3D(0.5, 0.5, 0.5), 1))
 
+n = len(par)
+dt = 1
+
+# Plotting the simulation
+fig = plt.figure()
+ax = fig.add_subplot(projection="3d")
+
+# Initialize initial positions
+positions = np.array([[p.position.x, p.position.y, p.position.z] for p in par])
+scat = ax.scatter(positions[:, 0], positions[:, 1], positions[:, 2])
+
+ax.set_xlim(0, 10)
+ax.set_ylim(0, 10)
+ax.set_zlim(0, 10)
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
 
 
+def init():
+    return scat,
 
+def update(frame):
+    for i in range(n):
+        par[i].calculate_acceleration(par)
+        par[i].update(dt)
+
+    positions = np.array([[p.position.x, p.position.y, p.position.z] for p in par])
+    scat._offsets3d = (positions[:, 0], positions[:, 1], positions[:, 2])
+    return scat,
+
+ani = FuncAnimation(fig, update, init_func=init, frames=100, interval=50, blit=False)
+
+plt.show()
